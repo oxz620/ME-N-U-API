2a363cfcead1d444ef9e673b4f0d8459421a84c9
Olivia Zhao <oxzhao24@gmail.com>
2025-01-04 16:09:17 -0600

    further setablishing server, including pool

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors=require("cors");
+const { Pool } = require("pg");
 
 require("dotenv").config();
 
@@ -8,13 +9,22 @@ const app =express();
 app.use((cors()));
 app.use(express.json());
 
-app.get("/", (req,res) => {
-    res.json({ message: "Welcome to your Express server" });
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+
+app.get('/users', async (req,res) => {
+    const result = await pool.query('SELECT * FROM users');
+    res.json(result.rows);
 })
 
 const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 module.exports=app;
