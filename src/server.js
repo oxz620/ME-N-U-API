a162027568fbed3afd4e3801ae3aeb399df4d586
Olivia Zhao <oxzhao24@gmail.com>
2025-01-04 16:35:53 -0600

    more edits to server to try to get users

--- src/server.js
+++ src/server.js
@@ -10,16 +10,15 @@ app.use(express.json());
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     user: process.env.USERNAME,
-    host: process.env.HOST,
     password: process.env.PASSWORD,
-    port: process.env.PORT || 3005,
+    port: process.env.PORT || 5436,
     ssl: {
         rejectUnauthorized: false,
     }
   });
 
   app.get('/', async (req,res) => {
-    res.json({ message: "hello world!"});
+    res.json({ message: "This is my server!!"});
 })
 
 app.get('/users', async (req,res) => {
@@ -27,7 +26,7 @@ app.get('/users', async (req,res) => {
     res.json(result.rows);
 })
 
-const PORT= process.env.PORT || 3005;
+const PORT= process.env.PORT || 5436;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
